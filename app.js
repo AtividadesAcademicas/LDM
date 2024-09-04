a003c314f5b6da5baf0f2f246082b9a333340cd8
Pessinaaa <pedro.pessina99@gmail.com>
2024-09-04 11:04:38 -0300

    aula 4

--- app.js
+++ app.js
@@ -15,12 +15,39 @@ app.get('/', function(req, res) {
 
 app.post('/cadastrar', function(req, res) {
     post.create({
-        nome: req.body.nome
+        nome: req.body.nome,
+        endereco: req.body.endereco,
+        bairro: req.body.bairro,
+        cep: req.body.cep,
+        cidade: req.body.cidade,
+        estado: req.body.estado,
+        telefone: req.body.telefone,
+        celular: req.body.celular
     }).then(function() {
         res.redirect('/')
     }).catch(function(error) {
         res.send("Erro: " + error)
     })
+})
+
+app.get('/consulta', function(req, res) {
+    post.findAll().then(function(posts) {
+        res.render("consulta", {posts: posts})
+        console.log(post)
+    }).catch(function(error) {
+        console.log("Erro ao carregar dados do banco: " + error)
+    })
+})
+
+app.get("/editar/:id", function(req, res) {
+    post.findAll({where: {"id": req.params.id}}).then(function(posts) {
+        res.render("editar", {posts: posts})
+    }).catch(function(error) {
+        console.log("Erro ao carregar dados do banco: " + error)
+    })
+})
+
+app.get("/excluir", function(req, res) {
 })
 
 app.listen(8081, function() {
